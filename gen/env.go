6178ae7a777d3d3858525ef63678f949cc0b6c9f
Kevin Wang <kevin@xorkevin.com>
2019-06-04 23:20:04 -0700

    Move parseArgs to parser file

--- gen/env.go
+++ gen/env.go
@@ -3,8 +3,89 @@ package gen
 import (
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
+
+func parseArgs(directive string, forgeenv map[string]string) ([]string, error) {
+	args := []string{}
+	for text := directive; len(text) > 0; text = strings.TrimLeft(text, " \t") {
+		replace := true
+		strmode := false
+		doublequote := true
+
+		switch text[0] {
+		case '\'':
+			replace = false
+			strmode = true
+			doublequote = false
+		case '"':
+			strmode = true
+		}
+
+		var arg string
+		if strmode {
+			found := false
+			for i := 1; i < len(text); i++ {
+				ch := text[i]
+				if ch == byte('\\') {
+					i++
+					continue
+				}
+				if doublequote && ch == '"' || !doublequote && ch == '\'' {
+					found = true
+					k := i + 1
+					if doublequote {
+						a, err := strconv.Unquote(text[0:k])
+						if err != nil {
+							return nil, errStringMisquote
+						}
+						arg = a
+					} else {
+						arg = text[1:i]
+					}
+					text = text[k:]
+					break
+				}
+			}
+			if !found {
+				return nil, errUnclosedString
+			}
+		} else {
+			k := strings.IndexAny(text, " \t")
+			if k < 0 {
+				k = len(text)
+			}
+			arg = text[0:k]
+			text = text[k:]
+			if strings.Contains(arg, "${") {
+				i := strings.IndexAny(text, "}")
+				if i < 0 {
+					return nil, errUnclosedBrace
+				}
+				k = i + 1
+				arg += text[0:k]
+				text = text[k:]
+				k = strings.IndexAny(text, " \t")
+				if k < 0 {
+					k = len(text)
+				}
+				arg += text[0:k]
+				text = text[k:]
+			}
+		}
+
+		if replace {
+			a, err := replaceEnvVar(arg, forgeenv)
+			if err != nil {
+				return nil, err
+			}
+			arg = a
+		}
+		args = append(args, arg)
+	}
+	return args, nil
+}
 
 const (
 	envvardefaultSeparator = ":-"
