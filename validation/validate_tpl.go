b4d7368b2026792d0c57b56fb5ae63bd14b0da4a
Kevin Wang <kevin@xorkevin.com>
2019-07-14 18:08:08 -0700

    Fix validation formatting

--- validation/validate_tpl.go
+++ validation/validate_tpl.go
@@ -2,17 +2,17 @@ package validation
 
 const templateValidate = `
 func (r {{.Ident}}) {{.Prefix}}() error {
-	{{$prevalid := .PrefixValid}}
-	{{$prehas := .PrefixHas}}
-	{{range .Fields}}
-	{{$fnp := $prevalid}}
-	{{if .Has}}
-		{{$fnp = $prehas}}
-	{{end}}
+	{{- $prevalid := .PrefixValid -}}
+	{{- $prehas := .PrefixHas -}}
+	{{- range .Fields -}}
+	{{- $fnp := $prevalid -}}
+	{{- if .Has -}}
+		{{- $fnp = $prehas -}}
+	{{- end }}
 	if err := {{$fnp}}{{.Key}}(r.{{.Ident}}); err != nil {
 		return err
 	}
-	{{end}}
+	{{- end }}
 	return nil
 }
 `
