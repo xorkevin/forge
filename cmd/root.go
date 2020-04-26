da731d88b0adeaa1b4e5aa13d682fd05d4b0a175
Kevin Wang <kevin@xorkevin.com>
2020-04-26 11:42:27 -0700

    Remove go homedir dep

--- cmd/root.go
+++ cmd/root.go
@@ -2,11 +2,9 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
 )
 
 const (
@@ -55,15 +53,15 @@ func initConfig() {
 		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
+		// Find config directory.
+		cfgdir, err := os.UserConfigDir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".forge" (without extension).
-		viper.AddConfigPath(home)
+		// Search config in XDG_CONFIG_HOME directory with name ".forge" (without extension).
+		viper.AddConfigPath(cfgdir)
 		viper.SetConfigName(".forge")
 	}
 
--- go.mod
+++ go.mod
@@ -3,7 +3,6 @@ module xorkevin.dev/forge
 go 1.12
 
 require (
-	github.com/mitchellh/go-homedir v1.1.0
 	github.com/spf13/cobra v1.0.0
 	github.com/spf13/viper v1.6.3
 )
